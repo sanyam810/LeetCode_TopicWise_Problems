3db4f7cd06f59275292569058810006684b75100
Sanyam Saini <72260960+sanyam810@users.noreply.github.com>
2022-04-08 22:30:04 +0530

    LeetCode_Problem_No.1464
    Given the array of integers nums, you will choose two different indices i and j of that array.
    Return the maximum value of (nums[i]-1)*(nums[j]-1).

---
+++ Heap (Priority Queue)/Maximum_product_of_2_elements_in_an_array.cpp
@@ -0,0 +1,27 @@
+/*
+
+Given the array of integers nums, you will choose two different indices i and j of that array.
+Return the maximum value of (nums[i]-1)*(nums[j]-1).
+
+Example:
+
+Input: nums = [3,4,5,2]
+Output: 12 
+Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12. 
+
+
+*/
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        
+        priority_queue<int>pq{nums.begin(),nums.end()};
+        
+        int i=pq.top();
+        pq.pop();
+        int j=pq.top();
+        
+        return (i-1)*(j-1);
+        
+    }
+};
