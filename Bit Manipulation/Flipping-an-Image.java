6d52b746394c22d285bc66eea78f6e8f1cf43fe4
Sanyam Saini <72260960+sanyam810@users.noreply.github.com>
2022-05-08 20:28:34 +0530

    LeetCode-Problem-No.832
    Given an n x n binary matrix image, flip the image horizontally, then invert it, and return the resulting image.

---
+++ Bit Manipulation/Flipping-an-Image.java
@@ -0,0 +1,35 @@
+/*
+
+Given an n x n binary matrix image, flip the image horizontally, then invert it, and return the resulting image.
+
+To flip an image horizontally means that each row of the image is reversed.
+
+For example, flipping [1,1,0] horizontally results in [0,1,1].
+To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.
+
+For example, inverting [0,1,1] results in [1,0,0].
+ 
+
+Example 1:
+
+Input: image = [[1,1,0],[1,0,1],[0,0,0]]
+Output: [[1,0,0],[0,1,0],[1,1,1]]
+Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+
+*/
+
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+     
+        for(int[] i:image){
+            for(int j=0;j<(image[0].length+1)/2;j++){
+                int temp=i[j]^1;
+                i[j]=i[image[0].length-j-1]^1;
+                i[image[0].length-j-1]=temp;
+            }
+        }
+        return image;
+        
+    }
+}
