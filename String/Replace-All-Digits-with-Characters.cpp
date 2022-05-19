856d2d4508e524521b6230f03a20f7979ff32dd9
Sanyam Saini <72260960+sanyam810@users.noreply.github.com>
2022-05-20 02:27:31 +0530

    LeetCode-Problem-No.844
    You are given a 0-indexed string s that has lowercase English letters in its even indices and digits in its odd indices.
    There is a function shift(c, x), where c is a character and x is a digit, that returns the xth character after c.
    
    For example, shift('a', 5) = 'f' and shift('x', 0) = 'x'.
    For every odd index i, you want to replace the digit s[i] with shift(s[i-1], s[i]).
    
    Return s after replacing all digits. It is guaranteed that shift(s[i-1], s[i]) will never exceed 'z'.

---
+++ String/Replace-All-Digits-with-Characters.cpp
@@ -0,0 +1,32 @@
+/*
+You are given a 0-indexed string s that has lowercase English letters in its even indices and digits in its odd indices.
+There is a function shift(c, x), where c is a character and x is a digit, that returns the xth character after c.
+
+For example, shift('a', 5) = 'f' and shift('x', 0) = 'x'.
+For every odd index i, you want to replace the digit s[i] with shift(s[i-1], s[i]).
+
+Return s after replacing all digits. It is guaranteed that shift(s[i-1], s[i]) will never exceed 'z'.
+
+
+Example :
+
+Input: s = "a1c1e1"
+Output: "abcdef"
+Explanation: The digits are replaced as follows:
+- s[1] -> shift('a',1) = 'b'
+- s[3] -> shift('c',1) = 'd'
+- s[5] -> shift('e',1) = 'f'
+
+*/
+
+class Solution {
+public:
+    string replaceDigits(string s) {
+        
+        for(int i=1;i<s.size();i+=2){
+            s[i]=char(s[i-1]+s[i]-'0');
+        }
+        return s;
+        
+    }
+};
